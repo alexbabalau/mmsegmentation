d8266c8753747558a8accd480c0de7fac1242f03
Alexandru Babalau <alexandru.babalau@yahoo.com>
2024-05-16 00:18:08 +0300

    wip

--- mmseg/models/backbones/focalnet.py
+++ mmseg/models/backbones/focalnet.py
@@ -22,6 +22,211 @@ from mmengine.utils import to_2tuple
 
 from ops_dcnv3 import modules as dcnv3
 
+class WindowMSA(BaseModule):
+    """Window based multi-head self-attention (W-MSA) module with relative
+    position bias.
+
+    Args:
+        embed_dims (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        window_size (tuple[int]): The height and width of the window.
+        qkv_bias (bool, optional):  If True, add a learnable bias to q, k, v.
+            Default: True.
+        qk_scale (float | None, optional): Override default qk scale of
+            head_dim ** -0.5 if set. Default: None.
+        attn_drop_rate (float, optional): Dropout ratio of attention weight.
+            Default: 0.0
+        proj_drop_rate (float, optional): Dropout ratio of output. Default: 0.
+        init_cfg (dict | None, optional): The Config for initialization.
+            Default: None.
+    """
+
+    def __init__(self,
+                 embed_dims,
+                 num_heads,
+                 window_size,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 attn_drop_rate=0.,
+                 proj_drop_rate=0.,
+                 init_cfg=None):
+
+        super().__init__(init_cfg=init_cfg)
+        self.embed_dims = embed_dims
+        self.window_size = window_size  # Wh, Ww
+        self.num_heads = num_heads
+        head_embed_dims = embed_dims // num_heads
+        self.scale = qk_scale or head_embed_dims**-0.5
+
+        # define a parameter table of relative position bias
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1),
+                        num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+        # About 2x faster than original impl
+        Wh, Ww = self.window_size
+        rel_index_coords = self.double_step_seq(2 * Ww - 1, Wh, 1, Ww)
+        rel_position_index = rel_index_coords + rel_index_coords.T
+        rel_position_index = rel_position_index.flip(1).contiguous()
+        self.register_buffer('relative_position_index', rel_position_index)
+
+        self.qkv = nn.Linear(embed_dims, embed_dims * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop_rate)
+        self.proj = nn.Linear(embed_dims, embed_dims)
+        self.proj_drop = nn.Dropout(proj_drop_rate)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def init_weights(self):
+        trunc_normal_(self.relative_position_bias_table, std=0.02)
+
+    def forward(self, x, mask=None):
+        """
+        Args:
+
+            x (tensor): input features with shape of (num_windows*B, N, C)
+            mask (tensor | None, Optional): mask with shape of (num_windows,
+                Wh*Ww, Wh*Ww), value should be between (-inf, 0].
+        """
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads,
+                                  C // self.num_heads).permute(2, 0, 3, 1, 4)
+        # make torchscript happy (cannot use tensor as tuple)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        q = q * self.scale
+
+        #q, k - (B, num_heads, N, C // num_heads)
+        #q_reshaped = q.view()
+        q_expanded = q.unsqueeze(-2)  # (B, num_heads, N, 1, C)
+        k_expanded = k.unsqueeze(-3)  # (B, num_heads, 1, N, C)
+
+        # Compute element-wise multiplication between q and k for each channel
+        attn_scores = q_expanded * k_expanded # (B, num_heads, N, N, C)
+
+        attn_scores = attn_scores.transpose(-2, -1) #(B, num_heads, N, C, N)
+
+        # Apply softmax along the last dimension (C) to get attention weights for each channel
+        attn_weights = F.softmax(attn_scores, dim=-1) # (B, num_heads, N, N, C)
+
+        attn_weights = attn_weights.transpose(-2, -1)
+
+        # Use attention weights to aggregate the values tensor, weighted by the attention scores, for each channel
+        aggregated_values = (attn_weights * v.unsqueeze(-3)).sum(dim = -2)
+        #attn = (q @ k.transpose(-2, -1))
+
+
+        x = aggregated_values.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+    @staticmethod
+    def double_step_seq(step1, len1, step2, len2):
+        seq1 = torch.arange(0, step1 * len1, step1)
+        seq2 = torch.arange(0, step2 * len2, step2)
+        return (seq1[:, None] + seq2[None, :]).reshape(1, -1)
+
+class ShiftWindowMSA(BaseModule):
+    """Shifted Window Multihead Self-Attention Module.
+
+    Args:
+        embed_dims (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        window_size (int): The height and width of the window.
+        shift_size (int, optional): The shift step of each window towards
+            right-bottom. If zero, act as regular window-msa. Defaults to 0.
+        qkv_bias (bool, optional): If True, add a learnable bias to q, k, v.
+            Default: True
+        qk_scale (float | None, optional): Override default qk scale of
+            head_dim ** -0.5 if set. Defaults: None.
+        attn_drop_rate (float, optional): Dropout ratio of attention weight.
+            Defaults: 0.
+        proj_drop_rate (float, optional): Dropout ratio of output.
+            Defaults: 0.
+        dropout_layer (dict, optional): The dropout_layer used before output.
+            Defaults: dict(type='DropPath', drop_prob=0.).
+        init_cfg (dict, optional): The extra config for initialization.
+            Default: None.
+    """
+
+    def __init__(self,
+                 embed_dims,
+                 num_heads,
+                 window_size,
+                 shift_size=0,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 attn_drop_rate=0,
+                 proj_drop_rate=0,
+                 dropout_layer=dict(type='DropPath', drop_prob=0.),
+                 init_cfg=None):
+        super().__init__(init_cfg=init_cfg)
+
+        self.window_size = window_size
+        self.shift_size = shift_size
+        assert 0 <= self.shift_size < self.window_size
+
+        self.w_msa = WindowMSA(
+            embed_dims=embed_dims,
+            num_heads=num_heads,
+            window_size=to_2tuple(window_size),
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop_rate=attn_drop_rate,
+            proj_drop_rate=proj_drop_rate,
+            init_cfg=None)
+
+        self.drop = build_dropout(dropout_layer)
+
+    def forward(self, query, hw_shape):
+        B, L, C = query.shape
+        H, W = hw_shape
+        assert L == H * W, 'input feature has wrong size'
+        query = query.view(B, H, W, C)
+
+        # pad feature maps to multiples of window size
+        pad_r = (self.window_size - W % self.window_size) % self.window_size
+        pad_b = (self.window_size - H % self.window_size) % self.window_size
+        query = F.pad(query, (0, 0, 0, pad_r, 0, pad_b))
+        H_pad, W_pad = query.shape[1], query.shape[2]
+
+        # cyclic shift
+
+        shifted_query = query
+        attn_mask = None
+
+        # nW*B, window_size, window_size, C
+        query_windows = self.window_partition(shifted_query)
+        # nW*B, window_size*window_size, C
+        query_windows = query_windows.view(-1, self.window_size**2, C)
+
+        # W-MSA/SW-MSA (nW*B, window_size*window_size, C)
+        attn_windows = self.w_msa(query_windows, mask=attn_mask)
+
+        # merge windows
+        attn_windows = attn_windows.view(-1, self.window_size,
+                                         self.window_size, C)
+
+        # B H' W' C
+        shifted_x = self.window_reverse(attn_windows, H_pad, W_pad)
+        # reverse cyclic shift
+        if self.shift_size > 0:
+            x = torch.roll(
+                shifted_x,
+                shifts=(self.shift_size, self.shift_size),
+                dims=(1, 2))
+        else:
+            x = shifted_x
+
+        if pad_r > 0 or pad_b:
+            x = x[:, :H, :W, :].contiguous()
+
+        x = x.view(B, H * W, C)
+
+        x = self.drop(x)
+        return x
+
 def build_norm_layer(dim,
                      norm_layer,
                      in_format='channels_last',
@@ -105,19 +310,7 @@ class FocalContextGathering(BaseModule):
         self.gamma = nn.Parameter(layer_scale * torch.ones(dim),
                                        requires_grad=True)
 
-        self.dcn = core_op(
-            channels=dim,
-            kernel_size=kernel_size,
-            stride=stride,
-            pad=pad,
-            dilation=dilation,
-            group=group,
-            offset_scale=offset_scale,
-            act_layer=act_layer,
-            norm_layer=norm_layer,
-            dw_kernel_size=dw_kernel_size,  # for InternImage-H/G
-            center_feature_scale=center_feature_scale,
-            use_dcn_v4_op=use_dcn_v4_op)  # for InternImage-H/G
+        self.windows_msa = ShiftWindowMSA(dim, num_heads=4, window_size=kernel_size)
 
 
     def forward(self, x):
@@ -161,23 +354,10 @@ class FocalModulation(BaseModule):
             self.ln = nn.LayerNorm(dim)
 
         for k in range(self.focal_level):
-            kernel_size = self.focal_window
+            kernel_size = self.focal_window + k * self.focal_factor
             self.focal_layers.append(
-                        FocalContextGathering(dim,
-                                              kernel_size=kernel_size,
-                                              stride=1,
-                                              pad=kernel_size//2,
-                                              dilation=1,
-                                              group=1,
-                                              offset_scale=1.0,
-                                              act_layer='GELU',
-                                              norm_layer='LN',
-                                              dw_kernel_size=None,  # for InternImage-H/G
-                                              center_feature_scale=False,
-                                              use_dcn_v4_op=False,
-                                              core_op=core_op
-                                              )
-                )
+                        ShiftWindowMSA(dim, num_heads=4, window_size=kernel_size),
+            )
 
     def forward(self, x):
         """ Forward function.
